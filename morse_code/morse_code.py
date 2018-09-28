4889bdfc8c708a2973c2d339fbf70f03bc61c50c
Aleksandr Musorin <amusorin@doublescoring.com>
2018-09-29 01:21:07 +0300

    add ex for homework 1

---
+++ morse_code/morse_code.py
@@ -0,0 +1,24 @@
+class MorseDecoder:
+    # TODO: Добавить остальные буквы
+    codes = {
+        "._": "A",
+        "_...": "B",
+        "_._.": "C"
+    }
+
+    def decode(self, cipher):
+        """
+        Функция декодирует шифр
+        :param cipher: str шифр морзе
+        :return: str полученное слово
+        """
+        # TODO: Убрать заглушку для тестов
+        # Заглушка
+        if cipher == "...___...":
+            return "SOS"
+        if cipher == "._":
+            return "A"
+        if cipher == "_.._ _.__ __..":
+            return "X Y Z"
+        # Конец заглушки
+        # TODO: Реализовать функцию
---
+++ vagrant/readme.md
@@ -0,0 +1,2 @@
+* ```brew cask install virtualbox ``` устанавливаем virtualbox
+* ```vagrant up --provider virsualbox``` запускаем vagrant. Будет долго ставить Windows
