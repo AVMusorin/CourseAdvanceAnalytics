34c4e212b297070cffadc1f431aed133b3bfdfe9
Aleksandr Musorin <amusorin@doublescoring.com>
2018-11-17 14:38:22 +0300

    Реализация парсинга booking.com

--- booking/booking.py
+++ booking/booking.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class Booking:
+class Booking_com:
     def __init__(self, driver_path):
         # http://chromedriver.chromium.org/downloads
         self.driver = webdriver.Chrome(driver_path)
